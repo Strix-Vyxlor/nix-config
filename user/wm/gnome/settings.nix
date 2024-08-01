b06cbbcb846b9d6d1a94e4273e6408cbedd455cc
Strix-Vyxlor <strix.vyxlor@gmail.com>
2024-08-01 14:32:32 +0200

    added keymaps

--- user/wm/gnome/settings.nix
+++ user/wm/gnome/settings.nix
@@ -3,6 +3,27 @@
   dconf.settings = with lib.hm.gvariant; {
     "org/gnome/desktop/wm/keybindings" = {
       close = ["<Super>q"];
+      maximize = ["<Super>Page_Up"];
+      minimize = ["<Super>Page_Down"];
+    };
+
+    # custom keymaps
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>t";
+      command = "blackbox";
+      name = "terminal";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      binding = "<Super>f";
+      command = "nautilus";
+      name = "files";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      binding = "<Super>r";
+      command = "wofi";
+      name = "launcher";
     };
   };
 }
