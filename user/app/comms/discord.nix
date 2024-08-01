ca94a94527bc02249515ed2503b6494977fffff5
Strix-Vyxlor <strix.vyxlor@gmail.com>
2024-08-01 17:52:56 +0200

    unfree warning be gone

--- user/app/comms/discord.nix
+++ user/app/comms/discord.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 {
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "discord"
+  ];
+
   home.packages = [
     (pkgs.discord.override {
       withVencord = true;
