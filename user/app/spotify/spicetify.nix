95329af225259f35fc906eeaab8a3bacc6abfb17
Strix-Vyxlor <strix.vyxlor@gmail.com>
2024-08-01 16:41:58 +0200

    work

--- user/app/spotify/spicetify.nix
+++ user/app/spotify/spicetify.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, inputs, systemSettings, ... }:
 let
-  spicePkgs = inputs.spicetify-nix.packages.${systemSettings.system};
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${systemSettings.system};
 in {
   imports = [ inputs.spicetify-nix.homeManagerModules ];
 
