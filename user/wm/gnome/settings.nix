05f0763e96d839d31673020850e516538973a590
Strix-Vyxlor <strix.vyxlor@gmail.com>
2024-08-02 11:21:15 +0200

    added settings for extensions

--- user/wm/gnome/settings.nix
+++ user/wm/gnome/settings.nix
@@ -36,5 +36,62 @@
       center-new-windows = true;
       workspaces-only-on-primary = false;
     };
+
+    # extension settings
+    # blur my shell
+    "org/gnome/shell/blur-my-shell/panel" = {
+      static-blur = false;
+      unblur-in-overview = true;
+      sigma = mkIint32 20;
+      brightness = mkDouble 0.9;
+    };
+
+    "org/gnome/shell/blur-my-shell/dash-to-dock" = {
+      blur = true;
+      static-blur = false;
+      sigma = mkIint32 30;
+      brightness = mkDouble 0.6;
+      override-background = true;
+    };
+
+    "org/gnome/shell/blur-my-shell/applications" = {
+      blur = true;
+      enable-all = true;
+      dynamic-opacity = false;
+      blur-on-overview = false;
+      sigma = mkIint32 10;
+      opacity = mkIint32 200;
+      brightness = mkDouble 1.0;
+      blacklist = [ 
+        "Plank"
+        "com.desktop.ding"
+        "Conky" 
+        "Brave-browser" 
+        "org.gnome.Boxes"
+      ];
+    };
+
+    # vitals
+    "org/gnome/shell/extensions/vitals" = {
+      fixed-widths = true;
+      menu-centered = true;
+      position-in-panel = mkIint32 0;
+
+      show-battery = false;
+      show-storage = false;
+      show-system = true;
+      show-voltage = false;
+    };
+
+    # quick settings
+    "org/gnome/shell/extensions/quick-settings-tweaks" = {
+      add-dnd-quick-toggle-enable = false;
+      notifications-enabled = false;
+      user-removed-buttons = [
+        "PowerProfilesToggle" 
+        "KeyboardBrightnessToggle" 
+        "NightLightToggle"
+      ];
+    };
   };
 }
