e210cef953080ed8b9ca8242f0e64c43bb30ce07
Strix Vyxlor <strix.vyxlor@gmail.com>
2024-08-12 09:24:55 +0200

    may this work

--- system/lang/pio.nix
+++ system/lang/pio.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 {
-  services.udev.packages = [ 
-    pkgs.platformio-core.udev
-    pkgs.openocd
-  ];
+  services.udev = {
+    enable = true;
+    packages = [ 
+      pkgs.platformio-core.udev
+      pkgs.openocd
+    ];
+  };
 }
