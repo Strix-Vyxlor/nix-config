9c8298d0a62a1cbb214840c79ce138b6e8bd0f22
Strix Vyxlor <strix.vyxlor@gmail.com>
2024-08-13 14:10:25 +0200

    variable name

--- user/wm/hyprland/hypr.nix
+++ user/wm/hyprland/hypr.nix
@@ -71,7 +71,7 @@
 
     label {
       monitor =
-      text = Hello, Emmet
+      text = Hello, ${userSettings.name};
       color = rgb(''+config.lib.stylix.colors.base07-rgb-r+'',''+config.lib.stylix.colors.base07-rgb-g+'', ''+config.lib.stylix.colors.base07-rgb-b+'')
       font_size = 25
       font_family = ''+userSettings.font+''
