5dc82fe5accb2c994745034e3603d823730e4947
Strix Vyxlor <strix.vyxlor@gmail.com>
2024-08-13 11:48:13 +0200

    more colors

--- user/app/launcher/tofi.nix
+++ user/app/launcher/tofi.nix
@@ -11,17 +11,27 @@
       height = 300;
 
       prompt-color = "#${config.lib.stylix.colors.base05}";
-      prompt-background = "#${config.lib.stylix.colors.base01}";
+      prompt-background = "#${config.lib.stylix.colors.base00}";
       prompt-background-padding = "2, 7";
       prompt-background-corner-radius = 15;
 
       input-color = "#${config.lib.stylix.colors.base05}";
-      input-background = "#${config.lib.stylix.colors.base01}";
+      input-background = "#${config.lib.stylix.colors.base00}";
 
       selection-color = "#${config.lib.stylix.colors.base0E}";
       selection-background = "#${config.lib.stylix.colors.base02}"; 
       selection-background-padding = "2, 5";
-      selection-background-corner-radius = 20;
+      selection-background-corner-radius = 15;
+
+      default-result-color = "#${config.lib.stylix.colors.base06}";
+      default-result-background = "#${config.lib.stylix.colors.base00}";
+      default-result-background-padding = "2, 5";
+      default-result-background-corner-radius = 15;
+
+      alternate-result-color = "#${config.lib.stylix.colors.base06}";
+      alternate-result-background = "#${config.lib.stylix.colors.base00}";
+      alternate-result-background-padding = "2, 5";
+      alternate-result-background-corner-radius = 15;
 
       border-color = "#${config.lib.stylix.colors.base0D}";
       ouline-color = "#${config.lib.stylix.colors.base0C}";
