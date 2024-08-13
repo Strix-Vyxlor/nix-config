0d082cc5741d3f0b3ee9225e638b256131671be9
Strix Vyxlor <strix.vyxlor@gmail.com>
2024-08-13 11:27:29 +0200

    more theming

--- user/app/launcher/tofi.nix
+++ user/app/launcher/tofi.nix
@@ -6,10 +6,14 @@
       font = "${userSettings.font}";
 
       prompt-color = "#${config.lib.stylix.colors.base05}";
-      prompt-background = "#${config.lib.stylix.colors.base02}";
+      prompt-background = "#${config.lib.stylix.colors.base01}";
 
       input-color = "#${config.lib.stylix.colors.base05}";
-      input-background = "#${config.lib.stylix.colors.base02}";
+      input-background = "#${config.lib.stylix.colors.base01}";
+
+      border-color = "#${config.lib.stylix.colors.base0D}";
+      background-color = "#${config.lib.stylix.colors.base00}";
+
     };
   };
 }
