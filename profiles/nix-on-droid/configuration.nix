9e2bbfd72f4038e6593739415747754a7e63f953
Strix Vyxlor <strix.vyxlor@gmail.com>
2024-08-31 23:18:00 +0200

    configuration android

--- profiles/nix-on-droid/configuration.nix
+++ profiles/nix-on-droid/configuration.nix
@@ -1,5 +1,6 @@
-{ config, lib, pkgs, zix-pkg, systemSettings, userSettings, inputs, ... }:
+{config, lib, pkgs, zix-pkg, systemSettings, userSettings, inputs, ... }:
 {
+  
   # Simply install just the packages
   environment.packages = with pkgs; [
     # User-facing stuff that you really really want to have
@@ -30,7 +31,7 @@
   ];
 
   home-manager = {
-    backupFileExtension = ".hm-back";
+    backupFileExtension = ".hbk";
     useGlobalPkgs = true;
 
     config = ./home.nix;
@@ -40,6 +41,46 @@
       inherit userSettings;
       inherit inputs;
     };
+  };
+
+  android-integration = {
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+    termux-wake-lock.enable = true;
+    termux-wake-unlock.enable = true;
+    xdg-open.enable = true;
+  };
+
+  environment.motd = ''
+    nix is crazy
+  '';
+
+  terminal = {
+    colors = {
+      background = "#1e1e2e";  
+      foreground = "#cdd6f4";
+      cursor = "#cdd6f4";
+      color0 = "#1e1e2e";
+      color1 = "#f38ba8";
+      color4 = "#89b4fa";
+      color3 = "#f9e2af";
+      color2 = "#a6e3a1";
+      color5 = "#94e2d5";
+      color6 = "#fab387";
+      color7 = "#cba6f7";
+      color8 = "#45475a";
+      color9 = "#f38ba8";
+      color12 = "#89b4fa";
+      color11 = "#f9e2af";
+      color10 = "#a6e3a1";
+      color13 = "#94e2d5";
+      color14 = "#fab387";
+      color15 = "#cba6f7";
+    };
+    font = "${pkgs.nerdfonts}/share/fonts/truetype/NerdFonts/ZedMonoNerdFont-Regular.ttf";
+
   };
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
