e66c4ef18ff3ab26e387bcfb91b45691816d7e17
Strix Vyxlor <strix.vyxlor@gmail.com>
2024-09-19 09:55:16 +0200

    cashix

--- profiles/rpi/sd-image.nix
+++ profiles/rpi/sd-image.nix
@@ -14,6 +14,15 @@
   '';
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings = {
+    substituters = [
+      ""https://nix-community.cachix.org""
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   boot.plymouth.enable = true;
 
   networking.hostName = systemSettings.hostname;
