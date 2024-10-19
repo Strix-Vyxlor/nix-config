ce633fff7eaf15156b3a4518113f9087ddedfea3
Strix Vyxlor <strix.vyxlor@gmail.com>
2024-10-19 18:00:45 +0200

    new config

--- profiles/server/home.nix
+++ profiles/server/home.nix
@@ -20,15 +20,11 @@
     ../../user/app/console/tmux.nix
   ];
 
-  home.file.".config/zix/zix.conf" = {
+  home.file.".config/zix/conf.toml" = {
     text = ''
-      {
-        "flake": true,
-        "path": ".nix-config",
-        "hostname": "default",
-        "nix-on-droid": false,
-        "root-command": "doas",
-      }
+      flake_path = ".nix-config"
+      hostname = "default"
+      root_command = "doas"
     '';
     executable = false;
   };
