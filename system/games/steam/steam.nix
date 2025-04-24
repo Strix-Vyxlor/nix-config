a60ed412ee074bab687ffaac41d0bff1939cc43f
Strix Vyxlor <strix.vyxlor@gmail.com>
2025-04-24 10:54:48 +0200

    forgor to save

--- system/games/steam/steam.nix
+++ system/games/steam/steam.nix
@@ -11,45 +11,5 @@ in {
     extraPackages = [
       pkgs.mangohud
     ];
-    gamescopeSession = {
-      enable = true;
-      env = {
-        # mangohud
-        STEAM_MANGOAPP_PRESETS_SUPPORTED = "1";
-        STEAM_USE_MANGOAPP = "1";
-        STEAM_USE_DYNAMIC_VRS = "1";
-        STEAM_MANGOAPP_HORIZONTAL_SUPPORTED = "1";
-        STEAM_DISABLE_MANGOAPP_ATOM_WORKAROUND = "1";
-
-        #SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS = "0";
-        # misc
-        STEAM_ENABLE_VOLUME_HANDLER = "1";
-        STEAM_DISABLE_AUDIO_DEVICE_SWITCHING = "1";
-        STEAM_GAMESCOPE_FANCY_SCALING_SUPPORT = "1";
-        SRT_URLOPEN_PREFER_STEAM = "1";
-        #STEAM_MULTIPLE_XWAYLANDS = "1";
-        #STEAM_GAMESCOPE_DYNAMIC_FPSLIMITER = "1";
-        #STEAM_GAMESCOPE_HAS_TEARING_SUPPORT = "1";
-        STEAM_GAMESCOPE_NIS_SUPPORTED = "1";
-        #STEAM_GAMESCOPE_TEARING_SUPPORTED = "1";
-        QT_IM_MODULE = "steam";
-        GTK_IM_MODULE = "Steam";
-
-        XKB_DEFAULT_LAYOUT = "be";
-      };
-      args = [
-        "--mangoapp"
-        #"--steam"
-        #"--expose-wayland"
-      ];
-      steamArgs = [
-        "-steamos3"
-        "-steampal"
-        "-steamdeck"
-        "-gamepadui"
-        #"-tenfoot"
-        "-pipewire-dmabuf"
-      ];
-    };
   };
 }
