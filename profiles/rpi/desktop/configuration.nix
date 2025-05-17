2c27f02d399df8f05e04ff859edfac73f4c4f65b
Strix Vyxlor <strix.vyxlor@gmail.com>
2025-05-17 08:54:22 +0200

    rpi desktop profile

---
+++ profiles/rpi/desktop/configuration.nix
@@ -0,0 +1,97 @@
+{
+  pkgs,
+  lib,
+  branch,
+  ...
+}: {
+  imports = [
+    ../../system/hardware-configuration.nix
+  ];
+  strixos = {
+    inherit branch;
+    hostName = "pix";
+    user = {
+      username = "strix";
+      name = "Strix Vyxlor";
+    };
+    boot = {
+      loader = "rpi5";
+      plymouth.enable = true;
+    };
+    hardware = {
+      kernel = "rpi5";
+      graphics = true;
+      bluetooth = true;
+    };
+    network = {
+      manager = "network-manager";
+      avahi = true;
+    };
+    locale = {
+      timezone = "Europe/Brussels";
+      locale = "en_US.UTF-8";
+      consoleKeymap = "be-latin1";
+    };
+    programs = {
+      superuser = "doas";
+      git = true;
+      retroarch = {
+        enable = true;
+        withCores = cores:
+          with cores; [
+            dolphin
+            picodrive
+            genesis-plus-gx
+          ];
+        gamescopeSession = {
+          enable = true;
+        };
+      };
+      desktop = {
+        hyprland = {
+          enable = true;
+          xkb = {
+            layout = "be";
+            variant = "";
+            options = "";
+          };
+          nautilus = true;
+          keyring = "gnome-keyring";
+          hyprlock = false;
+        };
+        displayManager = {
+          displayManager = "regreet";
+          regreet.extraHyprlandConfig = ''
+            input {
+              touchpad {
+                disable_while_typing=false
+                natural_scroll=yes
+              }
+              accel_profile=linear
+              kb_layout=be
+              numlock_by_default=true
+              repeat_delay=350
+              repeat_rate=50
+            }
+          '';
+        };
+      };
+    };
+    services = {
+      timesync = "chrony";
+      ssh.enable = true;
+    };
+    style.theme.themeDir = ../../themes/nord;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    helix
+    wget
+    git
+    home-manager
+  ];
+
+  system.stateVersion = "25.05";
+}
---
+++ profiles/rpi/desktop/home.nix
@@ -0,0 +1,97 @@
+{
+  config,
+  pkgs,
+  branch,
+  ...
+}: {
+  strixos = {
+    inherit branch;
+    user = {
+      username = "strix";
+      name = "Strix Vyxlor";
+      email = "strix.vyxlor@gmail.com";
+    };
+    style = {
+      theme.themeDir = ../../themes/nord;
+      targets.btop = true;
+      desktop = {
+        enable = true;
+      };
+    };
+    xdg = {
+      enable = true;
+    };
+    shell = {
+      defaultShell = "nushell";
+      nushell = {
+        aliases = {
+          ll = "ls -l";
+          lla = "ls -la";
+          tree = "eza --icons --tree";
+          plaincat = "^cat";
+          cat = "bat --plain";
+          neofetch = "fastfetch";
+        };
+      };
+      prompt = {
+        prompt = "oh-my-posh";
+      };
+      integrations = {
+        vivid = true;
+        direnv = true;
+        zoxide = true;
+      };
+    };
+    programs = {
+      cli = {
+        git = {
+          enable = true;
+          gh = true;
+        };
+        tmux.enable = true;
+      };
+      editor.strixvim = true;
+    };
+    desktop = {
+      hyprland = {
+        enable = true;
+        hyprcursorTheme = "Nordzy-Hyprcursors";
+        keymap = "be";
+        monitors = [
+        ];
+        extraSettings = {
+          exec-once = [
+            "nm-applet"
+            "blueberry-tray"
+          ];
+        };
+        apps.waybar = {
+          temperature = true;
+        };
+      };
+    };
+  };
+
+  home.packages = with pkgs; [
+    gnused
+    gnugrep
+    htop
+    unzip
+    gzip
+    xz
+    file
+    gnutar
+    bat
+    eza
+    fzf
+    blueberry
+    networkmanagerapplet
+  ];
+
+  programs.zix = {
+    enable = true;
+    config.root_command = "doas";
+  };
+
+  home.stateVersion = "25.05";
+}
